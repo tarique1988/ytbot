1f7f1541ef44e072d9c37695bf68f87c7572214c
Tarique Ali Mirza <sajmtam@gmail.com>
2019-11-15 19:21:29 +0530

    Template created

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Created by: Tarique Ali Mirza <sajmtam@gmail.com>
+Date started: November 15, 2019
+Version: 0
+"""
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options, FirefoxProfile
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+
+def main():
+    # The code goes here!
+    print("Please add your code here!")
+
+if __name__ == '__main__':
+    main()
